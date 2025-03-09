121643d29956a2992d64a9f52b7fee1f7d4c789a
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-03-09 19:09:25 +0600

    useAuth custom hook done

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+
+const useAuth = () => {
+    const [isLoggedIn, setLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const changeLoggedInStatus = async () => {
+            const token = localStorage.getItem("authToken");
+            if (token) {
+                try {
+                    const response = await fetch("https://blogging-app-api-using-express-mongo-db-iwvr.vercel.app/api/v1/validate-token");
+                    if (!response.ok) {
+                        localStorage.removeItem("authToken");
+                        setLoggedIn(false);
+                    } else {
+                        setLoggedIn(true);
+                    }
+
+                } catch (err) {
+
+                }
+            } else {
+                setLoggedIn(false);
+            }
+        }
+        changeLoggedInStatus();
+        return { isLoggedIn };
+    }, []);
+}
