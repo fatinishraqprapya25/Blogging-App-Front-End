15d103f34f5d0edccd6ff47a291f415c0bc8c964
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-04-03 11:36:42 +0600

    updating comment feature done

--- src/components/blogs/SingleComment.jsx
+++ src/components/blogs/SingleComment.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import { Ellipsis } from 'lucide-react';
 import { jwtDecode } from "jwt-decode";
+import Button from "../common/Button";
 
 export default function Comment({ comment, setComments }) {
     const [showCommentBtns, setShowCommentBtns] = useState(false);
+    const [newComment, setNewComment] = useState("");
+    const [isEditing, setIsEditing] = useState(false);
 
     const decodedUser = () => {
         const token = localStorage.getItem("authToken");
@@ -36,8 +39,24 @@ export default function Comment({ comment, setComments }) {
         }
     }
 
-    const editComment = async () => {
-        
+    const editComment = async (oldText) => {
+        if (oldText !== newComment) {
+            const token = localStorage.getItem("authToken");
+            const authorization = `Bearer ${token}`;
+            const commentId = comment._id;
+            const data = JSON.stringify({ text: newComment });
+            const response = await fetch(`https://blogging-app-api-using-express-mongo-db-iwvr.vercel.app/api/v1/comments/${commentId}`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": authorization
+                },
+                body: data
+            });
+            const updatedComment = await response.json();
+            setIsEditing(false);
+            alert("Comment updated successfully, reload comments section to see changes!");
+        }
     }
 
     return <div
@@ -46,7 +65,12 @@ export default function Comment({ comment, setComments }) {
         <div>
 
             <p className="font-semibold text-gray-800">{comment.userName}</p>
-            <p className="text-gray-700 mt-1">{comment.text}</p>
+            <p className="text-gray-700 mt-1">
+                {!isEditing ? comment.text : <>
+                    <input className="border-2 py-0 px-2 rounded-md mr-2" onChange={(e) => setNewComment(e.target.value)} value={newComment} />
+                    <Button onClick={() => editComment(comment.text)}>Update</Button>
+                </>}
+            </p>
             <p className="text-xs text-gray-500 mt-2">
                 {comment.date}
             </p>
@@ -58,7 +82,10 @@ export default function Comment({ comment, setComments }) {
             </button>
 
             {showCommentBtns && <div>
-                <button onClick={editComment} className="text-blue-500 hover:underline block text-sm font-bold mt-[-5px] cursor-pointer">Edit</button>
+                <button onClick={() => {
+                    setIsEditing(true);
+                    setNewComment(comment.text);
+                }} className="text-blue-500 hover:underline block text-sm font-bold mt-[-5px] cursor-pointer">Edit</button>
                 <button onClick={deleteComment} className="text-red-500 hover:underline block text-sm font-bold my-0 mt-[-6px] cursor-pointer">Delete</button>
             </div>}
         </div>}
